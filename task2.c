1cf3bec399f37cf57a692258042d12b5f8a792c4
Андрей Лютиков <70757449+andrey-andreu@users.noreply.github.com>
2020-12-08 16:39:42 +0300

    Add files via upload

--- task2.c
+++ task2.c
@@ -12,30 +12,32 @@
 #include <ctype.h>
 
 int main(int argc, char** argv){
-    int fille1, fille2, count = 0, count_str, flag = 0, symb = 0, symb2 = 0, i = 0, k = 0;
+    int fille1, fille2, count = 0, count_str, flag = 0, fflag = 0, symb = 0, symb2 = 0, i = 0, k = 0;
     char c;
     fille1 = open(argv[1], O_RDONLY);
     fille2 = open(argv[1], O_WRONLY);
 
     while (read(fille1, &c, sizeof(char)) > 0){
-        count = 0;
         flag = 0;
         symb++;
-        symb2 = 0;
-        while (c != '\n'){
-            symb2++;
-            if (((c - '0') >= 0) && ((c - '9') <= 0)){
-                count++;
-            }
-            read(fille1, &c, sizeof(char));
+        symb2++;
+        if (c == '\n') fflag = 1;
+        if (((c - '0') >= 0) && ((c - '9') <= 0)){
+            count++;
         }
-        if (count == 2){
-            lseek(fille1, symb-symb2, SEEK_SET);
-            i = 0;
-            for (i = 0; i <= symb2; i++){
-                read(fille1, &c, sizeof(char));
-                write(fille2, &c, sizeof(char));
+        printf("%c", c);
+        if (fflag){
+            if (count == 2){
+                lseek(fille1, symb-symb2, SEEK_SET);
+                i = 0;
+                for (i = 0; i <= symb2; i++){
+                    read(fille1, &c, sizeof(char));
+                    write(fille2, &c, sizeof(char));
+                }
             }
+            count = 0;
+            fflag = 0;
+            symb2 = 0;
         }
     }
     ftruncate(fille2, symb);
