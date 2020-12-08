8e69de08de41cc7223d65996ab6319e911f8daac
Андрей Лютиков <70757449+andrey-andreu@users.noreply.github.com>
2020-12-08 16:08:15 +0300

    Add files via upload

--- task2.c
+++ task2.c
@@ -9,21 +9,36 @@
 #include <fcntl.h>
 #include <wait.h>
 #include <signal.h>
+#include <ctype.h>
 
 int main(int argc, char** argv){
-    int fille, count = 0;
+    int fille1, fille2, count = 0, count_str, flag = 0, symb = 0, symb2 = 0, i = 0;
     int c;
-    fille = open(argv[argc-1], O_RDWR, 0644);
-    read(fille, c, 1);
-    while (c != EOF){
+    fille1 = open(argv[1], O_RDONLY);
+    fille2 = open(argv[1], O_WRONLY);
+
+    while (read(fille1, &c, sizeof(char)) > 0){
         count = 0;
+        flag = 0;
+        symb++;
+        symb2 = 0;
         while (c != '\n'){
-            if (issnum(c)){
+            symb2++;
+            if (isdigit(c)){
                 count++;
             }
         }
-        if (count >= 2){
-            
+        if (count == 2){
+            printf("%d\n", symb-symb2);
+            lseek(fille1, symb-symb2, SEEK_SET);
+            i = 0;
+            for (i = 0; i <= symb2; i++){
+                read(fille1, &c, sizeof(char));
+                write(fille2, &c, sizeof(char));
+            }
         }
     }
+    ftruncate(fille2, symb);
+    close(fille1);
+    close(fille2);
 }
