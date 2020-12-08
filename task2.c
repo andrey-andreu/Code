26db3f4a2a8c0c9fcefb359cea9fd4bbd39ea905
Андрей Лютиков <70757449+andrey-andreu@users.noreply.github.com>
2020-12-08 16:27:42 +0300

    Add files via upload

--- task2.c
+++ task2.c
@@ -12,8 +12,8 @@
 #include <ctype.h>
 
 int main(int argc, char** argv){
-    int fille1, fille2, count = 0, count_str, flag = 0, symb = 0, symb2 = 0, i = 0;
-    int c;
+    int fille1, fille2, count = 0, count_str, flag = 0, symb = 0, symb2 = 0, i = 0, k = 0;
+    char c;
     fille1 = open(argv[1], O_RDONLY);
     fille2 = open(argv[1], O_WRONLY);
 
@@ -24,12 +24,12 @@ int main(int argc, char** argv){
         symb2 = 0;
         while (c != '\n'){
             symb2++;
-            if (isdigit(c)){
+            if (((c - '0') >= 0) && ((c - '9') <= 0)){
                 count++;
             }
+            read(fille1, &c, sizeof(char));
         }
         if (count == 2){
-            printf("%d\n", symb-symb2);
             lseek(fille1, symb-symb2, SEEK_SET);
             i = 0;
             for (i = 0; i <= symb2; i++){
