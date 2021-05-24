0de23a1625ddfba9d2913c55b09cbb43b8fcf362
Андрей Лютиков <70757449+andrey-andreu@users.noreply.github.com>
2021-05-24 22:25:29 +0300

    test

---
+++ testt.js
@@ -0,0 +1,28 @@
+var a = "wefwf";
+while(a == 1) {
+    write("wefwfklwfle");
+}
+var a;
+a = 3;
+
+
+write(" wfwfwf");
+write("swfwftr");
+
+do{
+    a = a + 4;
+}while (a < 32);
+
+for(;;){
+    a = a * 33;
+}
+
+var a;
+a = 2 = 3 = 4;
+
+var i = 0;
+for (i = 0; i < 3; i++){
+    a++;
+}
+// efwfauhguhugruigruihpgauhgpuiohafgoiahgoaihgoahfgoihfaohgoiahfoihgoisdhfgoihdighohgoihofghosahgo
+write(" wfwf wfwef wf wfwfwef"); write(a);
